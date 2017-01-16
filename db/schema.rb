89d9ef48106fd3dbc463acf41f5bd48fdfe1d202
Granit Arifi <granitarifi.23@gmail.com>
2017-01-16 23:58:48 +0000

    Added clean form tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111221620) do
+ActiveRecord::Schema.define(version: 20170116235828) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
