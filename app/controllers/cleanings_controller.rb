1b19f20ae2900705455700f3b66771756e43dedd
Granit Arifi <granitarifi.23@gmail.com>
2017-01-17 00:37:40 +0000

    Added cleanings form view, route, controller

---
+++ app/controllers/cleanings_controller.rb
@@ -0,0 +1,4 @@
+class CleaningsController < ApplicationController
+  def new
+  end
+end
--- db/migrate/20170116235828_clean_form.rb
+++ db/migrate/20170116235828_clean_form.rb
@@ -1,4 +1,19 @@
 class CleanForm < ActiveRecord::Migration[5.0]
   def change
+    create_table :cleanings do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone_number
+      
+      t.string :street_address
+      t.integer :apt_number
+      t.string :city
+      t.string :state
+      t.integer :zipcode
+      
+      t.integer :number_of_rooms
+      t.integer :number_of_bathrooms
+      
   end
 end
